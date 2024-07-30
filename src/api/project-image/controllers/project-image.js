50e06e72302b9c28b80594884968ead9bfbaab64
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-07-30 19:35:01 +0530

    Custom controller and route added for Project-images

--- src/api/project-image/controllers/project-image.js
+++ src/api/project-image/controllers/project-image.js
@@ -6,4 +6,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::project-image.project-image');
+module.exports = createCoreController('api::project-image.project-image', ({ strapi }) => ({
+  async getAllProjectImages() {
+    try {
+      const projectsData = await strapi.entityService.findMany('api::project-image.project-image', {
+        fields: ['image_title'],
+        populate: {
+          image_src: {
+            fields: ['url']
+          }
+        },
+      });
+      return projectsData;
+    } catch (error) {
+      console.error('Error while fetching project images:', error);
+      throw error;
+    }
+  },
+}));
+
