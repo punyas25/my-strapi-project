8e401681328ef75bf63640caa4862f2f82986ab5
Punyashree Shetty <punyashreeshetty@Punyashrees-MacBook-Air.local>
2024-07-31 13:25:38 +0530

    New table created & functions seperated to fetch all project data and single project data

--- src/api/project/routes/custom-project.js
+++ src/api/project/routes/custom-project.js
@@ -2,8 +2,16 @@ module.exports = {
   routes: [
     {
       method: 'GET',
-      path: '/projects/get-all-projects-data/:project_slug',
+      path: '/projects/get-all-projects-data/',
       handler: 'project.getAllProjects',
+      config: {
+        auth: false,
+      },
+    },
+    {
+      method: 'GET',
+      path: '/projects/get-project-info/:project_slug',
+      handler: 'project.getProjectInfo',
       config: {
         auth: false,
       },
